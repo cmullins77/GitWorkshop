05e8ee43a628981c24d282fbf8684762dd369429
cmullins77 <mullins.cassie@gmail.com>
2018-02-16 17:59:42 -0500

    Commit 2

--- Assets/Scripts/LightSwitch.cs
+++ Assets/Scripts/LightSwitch.cs
@@ -3,13 +3,13 @@
 
     public class LightSwitch : MonoBehaviour {
 
-        public GameObject gameObj;
+        public GameObject lightSwitch;
 
         // Update is called once per frame
         void Update() {
-            if (this.gameObj != null) {
+            if (this.lightSwitch != null) {
                 if (Input.GetKeyUp(KeyCode.Space))
-                    this.gameObj.SetActive(!this.gameObj.activeSelf);
+                    this.lightSwitch.SetActive(!this.lightSwitch.activeSelf);
             }
         }
     }
