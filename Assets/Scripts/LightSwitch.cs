cb97f69103fffae5b0b3a42af907d7b90df63c25
cmullins77 <mullins.cassie@gmail.com>
2018-02-16 17:48:21 -0500

    first commit

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,16 @@
+﻿namespace GitWorkshop {
+    using UnityEngine;
+
+    public class LightSwitch : MonoBehaviour {
+
+        public GameObject gameObj;
+
+        // Update is called once per frame
+        void Update() {
+            if (this.gameObj != null) {
+                if (Input.GetKeyUp(KeyCode.Space))
+                    this.gameObj.SetActive(!this.gameObj.activeSelf);
+            }
+        }
+    }
+}
